49d37740f9dc5c4a3516c97c00da8299b13bbea1
anand <anand.solucky@gmail.com>
2021-05-14 18:00:00 +0530

    updated package.json file

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  connectionString:
-    "postgres://hvggsnhydgvhek:7849f90c7045914247069740164b4aa5201ebb427d59e31e14d83c49180e6bf5@ec2-3-215-57-87.compute-1.amazonaws.com:5432/df88ai6eq5lknf",
+  connectionString: process.env.DATABASE_URL,
   // user: "me",
   // host: "localhost",
   // database: "api",
