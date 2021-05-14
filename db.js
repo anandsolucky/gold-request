2a80efb4d3dbd7231ffcaea62173c9c123ab0abf
anand <anand.solucky@gmail.com>
2021-05-14 22:34:21 +0530

    commited

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // user: "me",
   // host: "localhost",
   // database: "api",
-  // password: "123456",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
   // port: 5432,
 });
-
 module.exports = pool;
